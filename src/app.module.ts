9d19dc525cfbb864bff74c99affa7aeddf93b252
Ivan Soplyakov <VanyakaCompany@yandex.ru>
2025-10-04 10:59:57 +0300

    Добавил конфиг

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { validationSchema } from '../config/validation';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'), 
+    ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: `config/.env.${process.env.NODE_ENV || 'development'}`,
+        validationSchema,
+    }),
+    MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGO_URI'),
+        }),
+    }),
     CustomersModule,
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.use(bodyParser.text({ type: 'application/xml' }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(configService.get<number>('PORT') as number);
 }
 bootstrap();
